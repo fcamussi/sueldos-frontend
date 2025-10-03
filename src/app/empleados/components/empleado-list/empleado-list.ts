3081f9b5f9cfd9f5272a01e12fef796cd8a3de15
Fernando Camussi <fcamussi@gmail.com>
2025-10-03 14:22:15 -0300

    dialogo confirmacion eliminar empleado

--- src/app/empleados/components/empleado-list/empleado-list.ts
+++ src/app/empleados/components/empleado-list/empleado-list.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Empleado} from '../../../models/empleado';
 import {EmpleadoServiceMock} from '../../../services/empleado-service-mock';
 import {Router} from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-empleado-list',
@@ -28,7 +29,24 @@ export class EmpleadoList implements OnInit {
   }
 
   onEliminar(id: number) {
-    console.log('Eliminar empleado:', id);
+    const empleado = this.empleados.find(empleado  => empleado.id === id);
+    const nombreApellido = empleado?.nombre + ' ' + empleado?.apellido;
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: `Eliminar a ${nombreApellido}`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, eliminar',
+      cancelButtonText: 'Cancelar',
+      customClass: {
+        confirmButton: 'btn btn-danger',
+        cancelButton: 'btn btn-secondary'
+      },
+    }).then(result => {
+      if (result.isConfirmed) {
+        console.log('Eliminar empleado:', id);
+      }
+    });
   }
 
 }
